fcf7248f3dc33641626b4d46153be50ddabcf2ff
manaheeljamil <manaheeljamil20@gmail.com>
2025-07-26 11:14:20 +0500

    monogo db

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const app = express()
-const router = require("./router/auth-router")
-app.use(express.json())
-app.use("/api/auth",router)
+const mongoose = require('mongoose')
+// const router = require("./router/auth-router")
+// app.use(express.json())
+// app.use("/api/auth",router)
+mongoose.connect("mongodb://localhost:27017")
+const User = require("./usermodule")
+async function insert(){
+    await User.create({
+        name:"manaheel24",
+        email:"anaheel20@gmal.com"
+    })
+}
+insert()
 const PORT = 5000
 app.listen(PORT,()=>{
     console.log(`server is running : ${PORT}`)
